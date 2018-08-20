eb67740a0791e64241717159fa5490a77d030027
ADingDing <dingdong246@sina.com>
2018-08-20 10:12:33 +0800

    Push to Upstream 首次使用

--- src/com/senter/tools/MD5.java
+++ src/com/senter/tools/MD5.java
@@ -16,6 +16,7 @@ public class MD5 {
 		System.out.println("offline change MD5 class!!");
 		System.out.println("offline change 2ed");
 		System.out.println("conflic test 本地第二次");
+		System.out.println("Push to upstream first time");
  
 		System.out.println("online change MD5 class");
 		System.out.println("online change2 ");
