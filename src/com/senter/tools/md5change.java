b2d56d85ae6f3d24684d1498a853ea46108de8b2
ADingDing <dingdong246@sina.com>
2018-08-20 08:44:33 +0800

    md5changsomethings

---
+++ src/com/senter/tools/md5change.java
@@ -0,0 +1,16 @@
+package com.senter.tools;
+
+
+    /**
+    * @ClassName: md5change
+    * @Description: TODO(类的描述)
+    * @author Dingliang
+    * @date 2018年8月20日
+    */
+
+public class md5change {
+	public static void main(String[] args) {
+		System.out.println("hello world!!!");
+	}
+
+}
